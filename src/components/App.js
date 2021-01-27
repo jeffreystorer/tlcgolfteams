7c066c4cb12bf1e0382ade1e1cdd2b7eebe8f323
jstorer <jeffrey.storer@gmail.com>
2021-01-27 18:23:03 -0500

    1/27 working on pdf

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,77 @@
-import React from 'react';
-import '../styles/App.css';
-import Header from './Header';
-import LineupPage from './LineupPage';
+import * as React from "react"
+import { ReactPhotoCollage } from "react-photo-collage"
+import cat from "../lineup.jpg"
+import ButtonDownloadScreenshot from "./ButtonDownloadScreenshot"
+import "../styles/App.css"
 
 export default function App() {
+  const img = new Image()
+  img.src = cat
+  let factor = 1.5
+  let width = img.width * 2 * factor
+  let widthPx = width.toString() + "px"
+  let height = img.height * factor
+  let heightPx = height.toString() + "px"
+  let imgDimensions = { width: width, height: height * 2 }
+  /*  img.onload = function () {
+    alert(this.width + "x" + this.height)
+  } */
+  let lineupTitle = "Two by Two"
+  let lineup = {
+    game: "Current Game",
+    course: "dc",
+  }
+  const setting = {
+    width: widthPx,
+    height: [heightPx, heightPx],
+    layout: [2, 2],
+    photos: [{ src: cat }, { src: cat }, { src: cat }, { src: cat }],
+    showNumOfRemainingPhotos: false,
+  }
+
   return (
     <>
-      <Header />
-      <LineupPage />
+      <div id="div-collage" className="background-white center">
+        <ReactPhotoCollage {...setting} />
+      </div>
+      <div className="center">
+        <br></br>
+        <br></br>
+
+        <ButtonDownloadScreenshot
+          dimensions={imgDimensions}
+          title={lineupTitle}
+          game={lineup.game}
+          course={lineup.course.toUpperCase()}
+          element="div-collage"
+          format="PDF"
+          page="Lineup2x2"
+        />
+        <br></br>
+        <br></br>
+
+        <ButtonDownloadScreenshot
+          dimensions={imgDimensions}
+          title={lineupTitle}
+          game={lineup.game}
+          course={lineup.course.toUpperCase()}
+          element="div-collage"
+          format="JPEG"
+          page="Lineup2x2"
+        />
+        <br></br>
+        <br></br>
+
+        <ButtonDownloadScreenshot
+          dimensions={imgDimensions}
+          title={lineupTitle}
+          game={lineup.game}
+          course={lineup.course.toUpperCase()}
+          element="div-collage"
+          format="PNG"
+          page="Lineup2x2"
+        />
+      </div>
     </>
   )
 }
-
--- src/components/ButtonDownloadScreenshot.js
+++ src/components/ButtonDownloadScreenshot.js
@@ -1,8 +1,10 @@
 import React from "react"
 import domtoimage from "dom-to-image"
 import { saveAs } from "../functions/fileSaver.js"
+import { jsPDF } from "jspdf"
 
 const ButtonDownLoadScreenshot = ({
+  dimensions,
   title,
   game,
   course,
@@ -10,32 +12,103 @@ const ButtonDownLoadScreenshot = ({
   format,
   page,
 }) => {
+  const PAPER_DIMENSIONS = {
+    width: 8.5,
+    height: 11,
+  }
+
+  const PAPER_RATIO = PAPER_DIMENSIONS.width / PAPER_DIMENSIONS.height
+  console.log("😊😊 dimensions", dimensions)
+  const imageDimensions = (dimensions) => {
+    console.log("😊😊 dimensions.width", dimensions.width)
+    console.log("😊😊 dimensions.height", dimensions.height)
+    const isLandscapeImage = dimensions.width >= dimensions.height
+
+    // If the image is in landscape, the full width of A4 is used.
+    if (isLandscapeImage) {
+      return {
+        width: PAPER_DIMENSIONS.width,
+        height: PAPER_DIMENSIONS.width / (dimensions.width / dimensions.height),
+      }
+    }
+
+    // If the image is in portrait and the full height of A4 would skew
+    // the image ratio, we scale the image dimensions.
+    const imageRatio = dimensions.width / dimensions.height
+    if (imageRatio > PAPER_RATIO) {
+      const imageScaleFactor =
+        (PAPER_RATIO * dimensions.height) / dimensions.width
+
+      const scaledImageHeight = PAPER_DIMENSIONS.height * imageScaleFactor
+
+      return {
+        height: scaledImageHeight,
+        width: scaledImageHeight * imageRatio,
+      }
+    }
+
+    // The full height of A4 can be used without skewing the image ratio.
+    return {
+      width: PAPER_DIMENSIONS.height / (dimensions.height / dimensions.width),
+      height: PAPER_DIMENSIONS.height,
+    }
+  }
+
+  console.log("😊😊 imageDimensions", imageDimensions)
+
+  const doc = new jsPDF({
+    orientation: "portrait",
+    unit: "in",
+    format: [8.5, 11],
+  })
   function handleClick() {
     let fileName
     if (page === "Teams") {
       fileName = "Teams for " + title
     } else {
-      fileName = "Course Handicaps for " + game + " at " + course
+      fileName = "Lineup Collage"
     }
-    if (format === "PNG") {
-      domtoimage.toBlob(document.getElementById(element)).then(function (blob) {
-        saveAs(blob, fileName + ".png")
-      })
-    }
-    if (format === "JPEG") {
-      domtoimage
-        .toJpeg(document.getElementById(element), { quality: 0.95 })
-        .then(function (dataUrl) {
-          var link = document.createElement("a")
-          link.download = fileName + ".jpeg"
-          link.href = dataUrl
-          link.click()
-        })
+    switch (format) {
+      case "PNG":
+        domtoimage
+          .toBlob(document.getElementById(element))
+          .then(function (blob) {
+            saveAs(blob, fileName + ".png")
+          })
+        break
+      case "JPEG":
+        domtoimage
+          .toJpeg(document.getElementById(element), { quality: 0.95 })
+          .then(function (dataUrl) {
+            var link = document.createElement("a")
+            link.download = title + ".jpeg"
+            link.href = dataUrl
+            link.click()
+          })
+        break
+      case "PDF":
+        domtoimage
+          .toJpeg(document.getElementById(element), { quality: 0.95 })
+          .then(function (dataUrl) {
+            doc.addImage(
+              dataUrl,
+              "JPEG",
+              (PAPER_DIMENSIONS.width - imageDimensions.width) / 2,
+              (PAPER_DIMENSIONS.height - imageDimensions.height) / 2,
+              imageDimensions.width,
+              imageDimensions.height
+            )
+            doc.setProperties({ title: title })
+            doc.save(title + ".pdf")
+          })
+        break
+      default:
+        break
     }
   }
   return (
     <button className="center" onClick={handleClick}>
-      Download Screenshot
+      Download Screenshot {format}
     </button>
   )
 }

6e5014e02d7a8f8afff981647006f699333ea3c4
jstorer <jeffrey.storer@gmail.com>
2021-01-28 17:59:57 -0500

    1/28 fixing up after testing pdf

--- src/components/App.js
+++ src/components/App.js
@@ -1,85 +1,13 @@
-import * as React from "react"
-import { ReactPhotoCollage } from "react-photo-collage"
-import cat from "../lineup.jpg"
-import ButtonDownloadScreenshot from "./ButtonDownloadScreenshot"
+import React from "react"
 import "../styles/App.css"
+import Header from "./Header"
+import LineupPage from "./LineupPage"
 
 export default function App() {
-  let lineupTitle = "Setting Div Dimensions Left"
-  const [zoom, setZoom] = React.useState()
-  const img = new Image()
-  img.src = cat
-  let factor = 2.0
-  let width = img.width * 2 * factor
-  let widthPx = width.toString() + "px"
-  let height = img.height * factor
-  let heightPx = height.toString() + "px"
-  let imgDimensions = { width: width, height: height * 2 }
-  const setting = {
-    width: widthPx,
-    height: [heightPx, heightPx],
-    layout: [2, 2],
-    photos: [{ src: cat }, { src: cat }, { src: cat }, { src: cat }],
-    showNumOfRemainingPhotos: false,
-  }
-  let styleWidth = imgDimensions.width + "px"
-  let styleHeight = imgDimensions.height + "px"
-
-  window.addEventListener("resize", getSizes, false)
-
-  function getSizes() {
-    let body = document.body
-    let currentZoom = body.clientWidth + "px X " + body.clientHeight + "px"
-    setZoom(currentZoom)
-  }
-
   return (
     <>
-      <p className="center">Zoom: {zoom}</p>
-      <p className="center">
-        2x2 Image--width: {imgDimensions.width} height: {imgDimensions.height}
-      </p>
-      <bt></bt>
-      <div className="center">
-        <ButtonDownloadScreenshot
-          dimensions={imgDimensions}
-          title={lineupTitle}
-          element="div-collage"
-          format="PDF"
-        />
-        <br></br>
-        <br></br>
-
-        <ButtonDownloadScreenshot
-          dimensions={imgDimensions}
-          title={lineupTitle}
-          element="div-collage"
-          format="JPEG"
-        />
-        <br></br>
-        <br></br>
-
-        <ButtonDownloadScreenshot
-          dimensions={imgDimensions}
-          title={lineupTitle}
-          element="div-collage"
-          format="PNG"
-        />
-
-        <br></br>
-        <br></br>
-      </div>
-      <br></br>
-      <div
-        id="div-collage"
-        className="background-white center"
-        style={{
-          width: { styleWidth },
-          height: { styleHeight },
-        }}
-      >
-        <ReactPhotoCollage {...setting} />
-      </div>
+      <Header />
+      <LineupPage />
     </>
   )
 }
--- src/components/ButtonDownloadScreenshot.js
+++ src/components/ButtonDownloadScreenshot.js
@@ -1,98 +1,41 @@
 import React from "react"
 import domtoimage from "dom-to-image"
 import { saveAs } from "../functions/fileSaver.js"
-import { jsPDF } from "jspdf"
 
-const ButtonDownLoadScreenshot = ({ dimensions, title, element, format }) => {
-  const PAPER_DIMENSIONS = {
-    width: 8.5,
-    height: 11,
-  }
-
-  const PAPER_RATIO = PAPER_DIMENSIONS.width / PAPER_DIMENSIONS.height
-  const imageDimensions = (dimensions) => {
-    // If the image is in portrait and the full height would skew
-    // the image ratio, we scale the image dimensions.
-    const imageRatio = dimensions.width / dimensions.height
-    if (imageRatio > PAPER_RATIO) {
-      const imageScaleFactor =
-        (PAPER_RATIO * dimensions.height) / dimensions.width
-
-      const scaledImageHeight = PAPER_DIMENSIONS.height * imageScaleFactor
-
-      return {
-        height: scaledImageHeight,
-        width: scaledImageHeight * imageRatio,
-      }
-    }
-
-    // The full height can be used without skewing the image ratio.
-    return {
-      width: PAPER_DIMENSIONS.height / (dimensions.height / dimensions.width),
-      height: PAPER_DIMENSIONS.height,
-    }
-  }
-
-  const doc = new jsPDF({
-    orientation: "portrait",
-    unit: "in",
-    format: [8.5, 11],
-  })
+const ButtonDownLoadScreenshot = ({
+  title,
+  game,
+  course,
+  element,
+  format,
+  page,
+}) => {
   function handleClick() {
-    switch (format) {
-      case "PNG":
-        createPNG()
-        break
-      case "JPEG":
-        createJPEG()
-        break
-      case "PDF":
-        createPDF()
-        break
-      default:
-        break
+    let fileName
+    if (page === "Teams") {
+      fileName = "Teams for " + title
+    } else {
+      fileName = "Course Handicaps for " + game + " at " + course
     }
-  }
-
-  function createJPEG() {
-    domtoimage
-      .toJpeg(document.getElementById(element), { quality: 0.95 })
-      .then(function (dataUrl) {
-        var link = document.createElement("a")
-        link.download = title + ".jpeg"
-        link.href = dataUrl
-        link.click()
+    if (format === "PNG") {
+      domtoimage.toBlob(document.getElementById(element)).then(function (blob) {
+        saveAs(blob, fileName + ".png")
       })
-  }
-
-  function createPNG() {
-    domtoimage.toBlob(document.getElementById(element)).then(function (blob) {
-      saveAs(blob, title + ".png")
-    })
-  }
-
-  function createPDF() {
-    domtoimage
-      .toJpeg(document.getElementById(element), { quality: 1.0 })
-      .then(function (dataUrl) {
-        let x, y, w, h
-        x = (PAPER_DIMENSIONS.width - imageDimensions(dimensions).width) / 2
-        y = (PAPER_DIMENSIONS.height - imageDimensions(dimensions).height) / 2
-        w = imageDimensions(dimensions).width
-        h = imageDimensions(dimensions).height
-        console.clear()
-        console.log("😊😊 x", x)
-        console.log("😊😊 y", y)
-        console.log("😊😊 w", w)
-        console.log("😊😊 h", h)
-        doc.addImage(dataUrl, "JPEG", x, y, w, h)
-        doc.setProperties({ title: title })
-        doc.save(title + ".pdf")
-      })
+    }
+    if (format === "JPEG") {
+      domtoimage
+        .toJpeg(document.getElementById(element), { quality: 0.95 })
+        .then(function (dataUrl) {
+          var link = document.createElement("a")
+          link.download = fileName + ".jpeg"
+          link.href = dataUrl
+          link.click()
+        })
+    }
   }
   return (
     <button className="center" onClick={handleClick}>
-      Download Screenshot {format}
+      Download Screenshot
     </button>
   )
 }
